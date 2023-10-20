c5359d15f2b8560ed6b5a897e47c14b0598a9dcb
Chirath Pansilu <chirathpansilu@gmail.com>
2023-10-20 14:10:52 +0530

    Color threshholding added

--- main.py
+++ main.py
@@ -30,3 +30,33 @@ plt.imshow(green_channel)
 plt.subplot(133)
 plt.imshow(blue_channel)
 plt.show()
+
+# Define a function to perform a color threshold
+def color_thresh(img, rgb_thresh=(0, 0, 0)):
+    color_select = np.zeros_like(img[:,:,0])
+    
+    above_thresh = (img[:,:,0] > rgb_thresh[0]) & (img[:,:,1] > rgb_thresh[1]) & (img[:,:,2] > rgb_thresh[2])
+    
+    color_select[above_thresh] = 1
+    
+    return color_select
+    
+    
+red_threshold = 160
+green_threshold = 160
+blue_threshold = 160
+
+rgb_threshold = (red_threshold, green_threshold, blue_threshold)
+
+colorsel = color_thresh(image, rgb_thresh=rgb_threshold)
+
+# Display the original image and binary               
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(21, 7), sharey=True)
+f.tight_layout()
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=40)
+
+ax2.imshow(colorsel, cmap='gray')
+ax2.set_title('Your Result', fontsize=40)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show()
